504c260b7a73c04442ff50abe1386dd0e275684f
CarlosEgberto <carlosegberto@gmail.com>
2024-09-06 16:35:30 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = 
+[
+  {
+    titulo: "Flamengo",
+    descricao:"O Mengão é conhecido por sua torcida apaixonada e sua história vitoriosa. Com um dos maiores estádios do mundo, o Maracanã, o Flamengo é um dos clubes mais populares do Brasil.",
+    cores:"preto e vermelho",
+    tags:"mais querido maracana",
+  },
+  {
+    titulo: "Vasco da Gama",
+    descricao:"O Gigante da Colina tem uma das maiores torcidas organizadas do país e uma história rica em títulos nacionais e internacionais.",
+    cores: "preto e branco",
+    tags: "vascao vascão sao januario", 
+  },
+  {
+    titulo: "Fluminense",
+    descricao:"O Tricolor tem uma tradição de revelar grandes talentos e é conhecido por seu estilo de jogo elegante",
+    cores:"verde, branco e grená",
+    tags: "fluzao fluzão maracana",
+  },
+  {
+    titulo: "Botafogo",
+    descricao:"O Glorioso é um dos clubes mais tradicionais do Brasil e tem uma história de lutas e superações",
+    cores: "preto e branco",
+    tags: "biriba bira estrela solitaria solitária nilton santos",
+  }
+];
